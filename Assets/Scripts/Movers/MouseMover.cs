cf5eb019ee02d0f3d17b0b7a478e2e67bde39824
ehsan <ehsan.kahrarian.2002@gmail.com>
2023-08-27 11:50:53 +0330

    Minor Code CleanUp

--- Assets/Scripts/Movers/MouseMover.cs
+++ Assets/Scripts/Movers/MouseMover.cs
@@ -6,27 +6,13 @@ namespace Movers
     public class MouseMover : MonoBehaviour
     {
         private Camera mainCamera;
-        private float CameraZDistance;
 
         private void Start()
         {
             mainCamera = Camera.main;
-            CameraZDistance =
-                mainCamera.WorldToScreenPoint(transform.position).z; //z axis of the game object for screen view
         }
 
-        private float counter = 0;
-
-        private void Update()
-        {
-            /*var screenPosition =
-                new Vector3(Input.mousePosition.x, Input.mousePosition.y, CameraZDistance); //z axis added to screen point 
-            var newWorldPosition =
-                mainCamera.ScreenToWorldPoint(screenPosition); //Screen point converted to world point
-
-            transform.position = newWorldPosition;*/
-        }
-        void FixedUpdate()
+        private void FixedUpdate()
         {
             var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray,out var hit, 100,LayerMask.GetMask("Default")))
